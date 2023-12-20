ee0c562ed52828d4fbe0b15c0e9e8ee04a2e6bae
Aleksei Zinchenko <a.zintus@gmail.com>
2023-12-20 15:32:15 +0000

    repro

--- esm-app/index.js
+++ esm-app/index.js
@@ -10,6 +10,7 @@ app.listen(PORT, HOST, function() {
   const host = addr.family === 'IPv6' ? `[${addr.address}]` : addr.address
   console.log('Server started at http://%s:%s', host, addr.port) 
 })
+import OpenAI from "openai";
 
 app.get('/', function(req, res) {
   res.send('Hello world')
@@ -18,6 +19,10 @@ app.get('/', function(req, res) {
 app.get('/user/:id', function(req, res) {
   res.send(`Reflected back the user id of ${req.params.id}`)
 })
+  const openai = new OpenAI(process.env.OPENAI_API_KEY);
+  openai.get("engines", (err, response) => {
+    console.log(response);
+  });
 
 app.get('/instrumentation-example', function(req, res) {
   const instrumentedUrl = normalizeUrl('test:me@example.com')
